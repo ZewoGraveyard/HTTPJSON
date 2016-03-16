4fbea7c59ccff59d8e3a40ad4638eff81878c891
antonmes <antonmes@users.noreply.github.com>
2016-03-16 08:26:04 +0300

    Swift 3 renaming

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "HTTPJSON",
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 3)
+		.Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 4)
 	]
 )
