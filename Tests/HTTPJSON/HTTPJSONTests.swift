40ef04c22f2a048f0d29f8a33eaaa072ab099553
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-24 23:01:53 -0300

    add tests

---
+++ Tests/HTTPJSON/HTTPJSONTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import HTTPJSON
+
+class HTTPJSONTests: XCTestCase {
+    func testReality() {
+        XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+    }
+}
+
+extension HTTPJSONTests {
+    static var allTests : [(String, HTTPJSONTests -> () throws -> Void)] {
+        return [
+           ("testReality", testReality),
+        ]
+    }
+}
