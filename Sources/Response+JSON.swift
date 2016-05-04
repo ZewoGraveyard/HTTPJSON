b08751b1c5d94e6c926ff1f989a04905dded71c3
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-05 01:30:25 +0900

    fixed line characters less than 100
    for houndci-bot

--- Sources/Response+JSON.swift
+++ Sources/Response+JSON.swift
@@ -27,7 +27,8 @@ import enum HTTP.Status
 import enum JSON.JSON
 
 extension Response {
-    public init(status: Status = .ok, headers: Headers = [:], json: JSON, didUpgrade: DidUpgrade? = nil) {
+    public init(status: Status = .ok, headers: Headers = [:], json: JSON,
+                didUpgrade: DidUpgrade? = nil) {
         var headers = headers
         headers["content-type"] = "application/json; charset=utf-8"
         self.init(
